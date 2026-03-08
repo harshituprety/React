7004ec00403572ffc1dbef0b17a554a926216964
Harshit Uprety <harshituprety17@outlook.com>
2026-03-08 14:31:29 +0530

    hex color

--- Projects/random-color/src/App.jsx
+++ Projects/random-color/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Index from "./Components/Index";
+import "./App.css";
 
 function App() {
   return <Index />;
--- Projects/random-color/src/Components/Index.jsx
+++ Projects/random-color/src/Components/Index.jsx
@@ -4,6 +4,28 @@ const Index = () => {
   const [typeOfColor, setTypeOfColor] = useState("hex");
   const [color, setColor] = useState("#000000");
 
+  function randomUtility(length) {
+    return Math.floor(Math.random() * length);
+  }
+
+  function randomHexColor() {
+    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[randomUtility(hex.length)];
+    }
+    console.log(hexColor);
+    setColor(hexColor);
+  }
+
+  function randomRgbColor() {
+    const r = randomUtility(256);
+    const g = randomUtility(256);
+    const b = randomUtility(256);
+
+    setColor(`rgb${r},${g},${b}`);
+  }
   return (
     <>
       <div
@@ -11,11 +33,26 @@ const Index = () => {
           width: "100vw",
           height: "100vh",
           background: color,
+          padding: 0,
+          margin: 0,
+          boxSizing: "border-box",
         }}
       >
-        <button>Create Hex Color</button>
-        <button>Create Rgb Color</button>
-        <button>Generate Random Color</button>
+        <button onClick={() => setTypeOfColor("hex")}>Create Hex Color</button>
+        <button onClick={() => setTypeOfColor("rgb")}>Create Rgb Color</button>
+        <button
+          onClick={typeOfColor === "hex" ? randomHexColor : randomRgbColor}
+        >
+          Generate Random Color
+        </button>
+        <button>
+          <div
+            style={{
+              display: "flex",
+              
+            }}
+          ></div>
+        </button>
       </div>
     </>
   );
