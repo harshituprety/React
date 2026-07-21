9bd2d3d7942a104453565440baac94eb6857dfff
DevHarsh <harshituprety17@outlook.com>
2026-07-22 00:05:03 +0530

    axios added

---
+++ fetch_api/src/App.jsx
@@ -0,0 +1,30 @@
+import Axios from "axios";
+import { useEffect, useState } from "react";
+
+const App = () => {
+  // fetch("https://catfact.ninja/fact")
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //   });
+  const [catFact, setCatFact] = useState("");
+
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact);
+    });
+  };
+
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
+
+  return (
+    <div className="App">
+      <button onClick={fetchCatFact}>Generate Cat Fact</button>
+      <p>{catFact}</p>
+    </div>
+  );
+};
+
+export default App;
